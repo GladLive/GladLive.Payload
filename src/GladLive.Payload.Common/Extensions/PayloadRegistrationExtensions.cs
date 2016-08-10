31e081b10e5e744dfe30261b8e8dd2fb87d10bd2
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-10 18:31:29 -0500

    Added Payload Registration Extensions

---
+++ src/GladLive.Payload.Common/Extensions/PayloadRegistrationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using GladNet.Payload;
+using GladNet.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GladLive
+{
+	/// <summary>
+	/// Extensions for payload registration.
+	/// </summary>
+	public static class PayloadRegistrationExtensions
+	{
+		/// <summary>
+		/// Register all the <see cref="PacketPayload"/>s in the <see cref="GladLive.Payload.Common"/> assembly.
+		/// </summary>
+		/// <typeparam name="TSerializerRegistryType"></typeparam>
+		/// <param name="registry">Serialization registry to register the payloads with.</param>
+		/// <returns>The registry to fluently chain off.</returns>
+		public static TSerializerRegistryType RegisterCommonGladLivePayload<TSerializerRegistryType>(this TSerializerRegistryType registry)
+			where TSerializerRegistryType : ISerializerRegistry
+		{
+			//Create a collection of the Common Payloads
+			IEnumerable<Type> payloadTypes = typeof(PayloadRegistrationExtensions).Assembly
+				.GetTypes()
+				.Where(t => typeof(PacketPayload).IsAssignableFrom(t)); //Finds all the payloads that can be assigned to PacketPayload
+
+			if (payloadTypes == null || payloadTypes.Count() == 0)
+				return registry;
+
+			foreach (Type pType in payloadTypes)
+			{
+				registry.Register(pType);
+			}
+
+			return registry;
+		}
+	}
+}
