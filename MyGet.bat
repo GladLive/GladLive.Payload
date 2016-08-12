7ad9a52cf08643b818344ace804f82283499f643
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-11 23:56:36 -0500

    Adjust Build Scripts

--- MyGet.bat
+++ MyGet.bat
@@ -1,2 +1,1 @@
-dotnet pack src/GladLive.Payload.Common/ -c Release
-dotnet pack src/GladLive.Payload.Profile/ -c Release
+dotnet pack src/*/ -c Release
--- pre-MyGet.bat
+++ pre-MyGet.bat
@@ -1,13 +1,3 @@
-cd ./src/GladNet.ASP.Server
-dotnet restore
-%nuget% update GladNet.ASP.Server.xproj -noninteractive
-cd ..
-cd ..
-
-cd ./src/GladNet.ASP.Formatters
-dotnet restore
-%nuget% update GladNet.ASP.Formatters.xproj -noninteractive
-cd ..
-cd ..
+dotnet restore src/*/project.json
 
 
